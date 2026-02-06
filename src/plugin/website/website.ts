ffc2b5ab201a6fb7ddbe3824fdc777e9a138273d
Arjan Seijs <arjan.seijs@gmail.com>
2026-02-06 13:38:57 +0100

    Skip files with publish false

--- src/plugin/website/website.ts
+++ src/plugin/website/website.ts
@@ -115,13 +115,23 @@ export class Website
 		return commonPath.length > 0 ? new Path(commonPath.join("/")).path : '';
 	}
 
+	public doPublish(file : TFile) {
+		if(!file) return false;
+		const frontmatter = app.metadataCache.getFileCache(file)?.frontmatter;
+		if (frontmatter && (frontmatter.publish === "false" || frontmatter.publish === false)) {
+			ExportLog.log(`Skipping file with publish=false: ${file.path}`);
+			return false;
+		}
+		return true;
+	}
+
 	public async load(files?: TFile[]): Promise<this>
 	{
 		ExportLog.resetProgress();
 		ExportLog.addToProgressCap((files?.length ?? 0));
 		ExportLog.addToProgressCap((files?.length ?? 0) * 0.1);
 
-		this.sourceFiles = files?.filter((file) => file) ?? [];
+		this.sourceFiles = files?.filter((file) => this.doPublish(file)) ?? [];
 
 		let rootPath = this.findCommonRootPath(this.sourceFiles);
 		this.exportOptions.exportRoot = rootPath;
